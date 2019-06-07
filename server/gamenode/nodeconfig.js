5343c92601d2acaa270346ee201de000cd31de04
Eric Lloyd <erlloyd@gmail.com>
2019-06-07 14:56:50 -0500

    Fix default mq url to be correct

--- server/gamenode/nodeconfig.js
+++ server/gamenode/nodeconfig.js
@@ -1,6 +1,6 @@
 var config = {
     secret: process.env.RINGSTEKI_SECRET,
-    mqUrl: process.env.RINGSTEKI_MQURL || 'mongodb://mongo:27017/ringsteki',
+    mqUrl: process.env.RINGSTEKI_MQURL || 'tcp://127.0.0.1:6000',
     socketioPort: process.env.SOCKET_IO_PORT || 9500, // This is the port for the game node to listen on
     nodeIdentity: process.env.RINGSTEKI_NODE_IDENTITY || 'test1', // This is the identity of the node,
     host: process.env.RINGSTEKI_NODE_HOST || 'localhost'
